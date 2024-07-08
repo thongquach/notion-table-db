f06459fedc2657ea113d8eaa2bc9061f10d462c6
Tom Quach <qtthong1997@gmail.com>
2024-07-08 23:03:04 +0700

    chore: adding test placeholder

---
+++ sample-app/src/filters/__test__/Filter.test.tsx
@@ -0,0 +1,17 @@
+describe("Filter", () => {
+  describe("RichTextFilter", () => {
+    it("should render correctly", () => {});
+
+    it("should behave correctly", () => {});
+  });
+
+  describe("MultiSelectFilter", () => {
+    it("should render correctly", () => {
+      // verify that the component renders correctly with: string operators and boolean operators
+    });
+
+    it("should behave correctly", () => {
+      // verify that the component behaves correctly with: string operators and boolean operators
+    });
+  });
+});
---
+++ sample-app/src/filters/__test__/index.test.tsx
@@ -0,0 +1,13 @@
+// TODO: Add tests for filters
+describe("Filters", () => {
+  it("should render correctly", () => {
+    // verify that the component renders correctly with different cases: empty filters, single filter, multiple filters
+    // verify by querying texts or snapshots
+  });
+
+  it("should behave correctly", () => {
+    // verify that the component behaves correctly when adding filter rule/group
+    // verify that changing the filter does update UI and value
+    // verify by simulating user interactions and checking the output texts
+  });
+});
